f329e755d71b5a894a277f6c37a5e39bb4094c63
ermish <philipermish@gmail.com>
2021-01-14 15:22:48 +0900

    added jest tests and linting

--- src/converter.tsx
+++ src/converter.tsx
@@ -10,85 +10,92 @@ interface File {
     data: string
 }
 
-interface FrontMatterAndJSX { frontMatterJsxString: string, jsxString: string }
-interface FrontMatterSplitFromMarkdown { frontMatterJsxString: string, markdownString: string }
+interface FrontMatterAndJSX {
+    frontMatterJsxString: string
+    jsxString: string
+}
+interface FrontMatterSplitFromMarkdown {
+    frontMatterJsxString: string
+    markdownString: string
+}
 
-
-  /**
-   * Converts Markdown files to JSX files, including any html in the markdown.
-   * Also, front-matter will be extracted in js variables in the react component.
-   * @param {string[]} `markdownFiles` Markdown strings to convert that may also include front-matter and html.
-   * @param {string} `outputFolderPath` Front matter to stringify.
-   * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
-   * @return {Promise<JsxFiles>} Files containing jsx 
-   */
-  export const convertMarkdownFilesToJSXFiles = async (markdownFiles: File[], reactContextName?: string) : Promise<File[]> => {
-    const files = await Promise.all(markdownFiles.map(async file => {
-
-        const frontMatterAndMarkdown = extractFrontMatter(file.data, reactContextName)
-        const jsxString = convertMarkdownAndHtmlToJsx(frontMatterAndMarkdown.markdownString)
-        return { fileName: file.fileName, data: jsxString }
-    }))
+/**
+ * Converts Markdown files to JSX files, including any html in the markdown.
+ * Also, front-matter will be extracted in js variables in the react component.
+ * @param {string[]} `markdownFiles` Markdown strings to convert that may also include front-matter and html.
+ * @param {string} `outputFolderPath` Front matter to stringify.
+ * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
+ * @return {Promise<JsxFiles>} Files containing jsx
+ */
+export const convertMarkdownFilesToJSXFiles = async (markdownFiles: File[], reactContextName?: string): Promise<File[]> => {
+    const files = await Promise.all(
+        markdownFiles.map(async file => {
+            const frontMatterAndMarkdown = extractFrontMatter(file.data, reactContextName)
+            const jsxString = convertMarkdownAndHtmlToJsx(frontMatterAndMarkdown.markdownString)
+            return { fileName: file.fileName, data: jsxString }
+        })
+    )
 
     return files
 }
 
-  /**
-   * Converts Markdown to JSX, including any html in the markdown.
-   * Also, front-matter will be extracted in js variables in the react component.
-   * @param {string[]} `markdownStrings` Markdown strings to convert that may also include front-matter and html.
-   * @param {string} `outputFolderPath` Front matter to stringify.
-   * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
-   * @return {Promise<FrontMatterAndJSX>} Front matter and Markdown JSX strings 
-   */
-export const convertMarkdownToJSX = async (markdownString: string, reactContextName?: string) : Promise<FrontMatterAndJSX> => {
+/**
+ * Converts Markdown to JSX, including any html in the markdown.
+ * Also, front-matter will be extracted in js variables in the react component.
+ * @param {string[]} `markdownStrings` Markdown strings to convert that may also include front-matter and html.
+ * @param {string} `outputFolderPath` Front matter to stringify.
+ * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
+ * @return {Promise<FrontMatterAndJSX>} Front matter and Markdown JSX strings
+ */
+export const convertMarkdownToJSX = async (markdownString: string, reactContextName?: string): Promise<FrontMatterAndJSX> => {
     const frontMatterAndMarkdown = extractFrontMatter(markdownString, reactContextName)
     const jsxString = convertMarkdownAndHtmlToJsx(frontMatterAndMarkdown.markdownString)
-    
-    return { 
-        frontMatterJsxString: frontMatterAndMarkdown.frontMatterJsxString, 
-        jsxString: jsxString 
+
+    return {
+        frontMatterJsxString: frontMatterAndMarkdown.frontMatterJsxString,
+        jsxString: jsxString
     }
 }
 
-const convertMarkdownAndHtmlToJsx = (markdownString: string) : string => {
-    let markdownJsx = ""
+const convertMarkdownAndHtmlToJsx = (markdownString: string): string => {
+    let markdownJsx = ''
     try {
         // @ts-ignore
         const jsxString = renderToStaticMarkup(<ReactMarkdownWithHtml children={mdFileMarkdownString} allowDangerousHtml />, markdownJsx)
     } catch (error) {
         console.log(`error converting markdown to jsx: ${error}`)
-        throw new error
+        throw new error()
     }
 
     return markdownJsx
 }
 
-
-const extractFrontMatter = (stringWithFrontMatter: string, reactContextName?: string) : FrontMatterSplitFromMarkdown => {
+const extractFrontMatter = (stringWithFrontMatter: string, reactContextName?: string): FrontMatterSplitFromMarkdown => {
     try {
         const { data, content } = matter(stringWithFrontMatter)
         const frontMatterJsxString = createFrontMatterJSXString(data, reactContextName)
 
         return {
-             frontMatterJsxString: frontMatterJsxString, 
-             markdownString: content
+            frontMatterJsxString: frontMatterJsxString,
+            markdownString: content
         }
     } catch (error) {
         console.log(`failed to extract front matter: ${error}`)
-        throw new error
+        throw new error()
     }
 }
 
-const createFrontMatterJSXString = (propsToAssign: Object, existingContextName?: string) : string => {
+const createFrontMatterJSXString = (propsToAssign: Object, existingContextName?: string): string => {
     const contextName = existingContextName ?? 'ShujiContext'
-    
-    const createContextString = existingContextName ? `
+
+    const createContextString = existingContextName
+        ? `
         export const ${contextName} = React.createContext(
             ...variables
         );
-    ` : ''       
-    
+    `
+        : ''
+
     const propAssignmentString = `
         for (const propName in propsToAssign) {
             window[contextName][propName] = propsToAssign[propName as keyof Object];
--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,26 @@ import { convertMarkdownFilesToJSXFiles } from './converter'
 import { getMdFilesFromFolder, writeJsxFiles } from './fileProcessor'
 
 interface Options {
-    inputFolderPath?: string,
-    outputFolderPath?: string,
+    inputFolderPath?: string
+    outputFolderPath?: string
     reactContextName?: string
 }
 
-const defaultOptions = {
+export const defaultOptions = {
     inputFolderPath: 'pages',
     outputFolderPath: 'jsxPages',
-    reactContextName: 'shuji',
+    reactContextName: 'shuji'
 }
 
-  /**
-   * Converts Markdown files to JSX files, including any html in the markdown.
-   * Also, front-matter will be extracted in js variables in the react component.
-   * @param {string} `inputFolderPath` path to folder to convert markdown files
-   * @param {string} `outputFolderPath` path to folder for outputting .jsx files
-   * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
-   * @return {Promise<JsxFiles>} files 
-   */
-  export const compile = async (options?: Options) : Promise<number> => {
+/**
+ * Converts Markdown files to JSX files, including any html in the markdown.
+ * Also, front-matter will be extracted in js variables in the react component.
+ * @param {string} `inputFolderPath` path to folder to convert markdown files
+ * @param {string} `outputFolderPath` path to folder for outputting .jsx files
+ * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
+ * @return {Promise<JsxFiles>} files
+ */
+export const compile = async (options?: Options): Promise<number> => {
     try {
         const userOptions = {
             ...defaultOptions,
@@ -32,7 +32,7 @@ const defaultOptions = {
         const jsxStrings = await convertMarkdownFilesToJSXFiles(mdFiles, userOptions.reactContextName)
         await writeJsxFiles(userOptions.outputFolderPath, jsxStrings)
 
-        return 1    
+        return 1
     } catch (error) {
         return 0
     }
