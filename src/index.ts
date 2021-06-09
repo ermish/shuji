22db5078be3636c2c4a4df09034b007c973f163a
ermish <philipermish@gmail.com>
2021-06-09 22:45:25 +0900

    added new option to return front matter object

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { convertMarkdownFilesToJSXFiles, convertMarkdownToJSX } from './converter'
+import { convertMarkdownFilesToJSXFiles, convertMarkdownToJSX, convertMarkdownToJSXAndObject, frontMatterModeEnum } from './transformer'
 import { getMdFilesFromFolder, writeJsxFiles } from './fileProcessor'
 
-const { performance, PerformanceObserver, PerformanceEntry  } = require("perf_hooks")
+const { performance, PerformanceObserver  } = require("perf_hooks")
 import { logger } from './logger'
 
 export interface Options {
@@ -46,7 +46,7 @@ perfObserver.observe({ entryTypes: ["measure"], buffer: true })
  * @param {Options} User defined options to override default values.
  * @return {Promise<number>} files
  */
- export const transformMarkdownString = async (markdownString: string, componentName: string, options?: Options): Promise<string> => {
+ export const transformMarkdownString = async (markdownString: string, componentName?: string, options?: Options): Promise<string> => {
     try {
         performance.mark('start-shuji')
 
@@ -56,11 +56,39 @@ perfObserver.observe({ entryTypes: ["measure"], buffer: true })
             frontMatterMode: options?.frontMatterMode?.toLowerCase() ?? defaultOptions.frontMatterMode
         }
 
-        const jsxString = await convertMarkdownToJSX(markdownString, componentName, userOptions.frontMatterMode, userOptions.reactHeadContextVarName, userOptions.reactHeadContextName)
+        const jsxString = await convertMarkdownToJSX(markdownString, userOptions.frontMatterMode as frontMatterModeEnum, userOptions.reactHeadContextVarName, userOptions.reactHeadContextName, componentName)
 
         return jsxString
     } catch (error) {
         return ''
+    } finally {
+        performance.mark('end-shuji')
+        performance.measure('shuji', 'start-shuji', 'end-shuji')
+    }
+}
+
+/**
+ * Transforms a Markdown string to JSX, including any html in the markdown.
+ * Also, front-matter will be extracted in js variables in the react component.
+ * @param {string} `markdownString` Markdown string to convert to a react component that may also include front-matter and html.
+ * @param {Options} User defined options to override default values.
+ * @return {Promise<number>} files
+ */
+ export const transformMarkdownStringWithFMObject = async (markdownString: string, componentName?: string, options?: Options): Promise<[string, object]> => {
+    try {
+        performance.mark('start-shuji')
+
+        const userOptions = {
+            ...defaultOptions,
+            ...options,
+            frontMatterMode: options?.frontMatterMode?.toLowerCase() ?? defaultOptions.frontMatterMode
+        }
+
+        const [jsxString, frontMatterObject] = await convertMarkdownToJSXAndObject(markdownString, userOptions.reactHeadContextVarName, userOptions.reactHeadContextName, componentName)
+
+        return [jsxString, frontMatterObject]
+    } catch (error) {
+        throw new Error(`Any error occurred transforming your markdown: ${error}`)
     } finally {
         performance.mark('end-shuji')
         performance.measure('shuji', 'start-shuji', 'end-shuji')
@@ -85,7 +113,7 @@ export const transformMarkdownFiles = async (options?: Options): Promise<number>
         }
 
         const mdFiles = await getMdFilesFromFolder(userOptions.inputPath)
-        const jsxStrings = await convertMarkdownFilesToJSXFiles(mdFiles, userOptions.frontMatterMode, userOptions.reactHeadContextVarName, userOptions.reactHeadContextName)
+        const jsxStrings = await convertMarkdownFilesToJSXFiles(mdFiles, userOptions.frontMatterMode as frontMatterModeEnum, userOptions.reactHeadContextVarName, userOptions.reactHeadContextName)
         await writeJsxFiles(userOptions.outputPath, jsxStrings, userOptions.deleteExistingOutputFolder)
 
         return 0
--- src/transformer.tsx
+++ src/transformer.tsx
@@ -11,14 +11,18 @@ interface File {
     data: string
 }
 
-interface FrontMatterAndJSX {
-    frontMatterJsxString: string
-    jsxString: string
-}
 interface FrontMatterSplitFromMarkdown {
-    frontMatterJsxString: string
+    frontMatterJsxString?: string
+    frontMatterObject?: any
     markdownString: string
     componentNameFromFrontMatter?: string
+}
+
+export enum frontMatterModeEnum {
+    object = 'object',
+    reactHead = 'reacthead',
+    reactHelmet = 'reacthelmet',
+    none = 'none'
 }
 
 /**
@@ -29,7 +33,7 @@ interface FrontMatterSplitFromMarkdown {
  * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
  * @return {Promise<JsxFiles>} Files containing jsx
  */
-export const convertMarkdownFilesToJSXFiles = async (markdownFiles: File[], frontMatterMode:string, reactContextVarName: string, reactContextName: string): Promise<File[]> => {
+export const convertMarkdownFilesToJSXFiles = async (markdownFiles: File[], frontMatterMode: frontMatterModeEnum, reactContextVarName: string, reactContextName: string): Promise<File[]> => {
     const files = await Promise.all(
         markdownFiles.map(async file => {
             const frontMatterAndMarkdown = extractFrontMatter(file.data, frontMatterMode, reactContextVarName, reactContextName)
@@ -51,12 +55,39 @@ export const convertMarkdownFilesToJSXFiles = async (markdownFiles: File[], fron
  * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
  * @return {Promise<string>} Front matter and Markdown JSX strings
  */
-export const convertMarkdownToJSX = async (markdownString: string, componentName: string, frontMatterMode:string, reactContextVarName: string, reactContextName: string): Promise<string> => {
+export const convertMarkdownToJSX = async (markdownString: string, frontMatterMode: frontMatterModeEnum, reactContextVarName: string, reactContextName: string, componentName?: string): Promise<string> => {
     const frontMatterAndMarkdown = extractFrontMatter(markdownString, frontMatterMode, reactContextVarName, reactContextName)
     const jsxString = convertMarkdownAndHtmlToJsx(frontMatterAndMarkdown.markdownString)
-    const reactComponentString = createJsxComponentString(componentName, jsxString, frontMatterMode, frontMatterAndMarkdown.frontMatterJsxString, reactContextName)
+    
+    if(componentName == null && frontMatterAndMarkdown.componentNameFromFrontMatter == null) {
+        throw new Error('You must provide a component name eithe through front matter or as a parameter')
+    }
+
+    const reactComponentString = createJsxComponentString(componentName ?? frontMatterAndMarkdown.componentNameFromFrontMatter as string, jsxString, frontMatterMode, frontMatterAndMarkdown.frontMatterJsxString, reactContextName)
 
     return reactComponentString
+}
+
+/**
+ * Converts Markdown to JSX, including any html in the markdown.
+ * Also, front-matter will be extracted in js variables in the react component.
+ * @param {string} `markdownString` Markdown string to convert to a react component that may also include front-matter and html.
+ * @param {componentName} `componentName` Name of new react component.
+ * @param {string} `reactContextVarName` name of react context variable name to assign front-matter variables to. like [shuji, setShuji] = useContext(...)
+ * @param {string} `reactContextName` name of react context object to assign front-matter variables to.
+ * @return {Promise[string, object]} Markdown JSX string and front matter object.
+ */
+export const convertMarkdownToJSXAndObject = async (markdownString: string, reactContextVarName: string, reactContextName: string, componentName?: string): Promise<[string, object]> => {
+    const frontMatterAndMarkdown = extractFrontMatter(markdownString, frontMatterModeEnum.object, reactContextVarName, reactContextName)
+    const jsxString = convertMarkdownAndHtmlToJsx(frontMatterAndMarkdown.markdownString)
+
+    if(componentName == null && frontMatterAndMarkdown.componentNameFromFrontMatter == null) {
+        throw new Error('You must provide a component name eithe through front matter or as a parameter')
+    }
+
+    const reactComponentString = createJsxComponentString(componentName ?? frontMatterAndMarkdown.componentNameFromFrontMatter as string, jsxString, frontMatterModeEnum.object, frontMatterAndMarkdown.frontMatterJsxString, reactContextName)
+
+    return [reactComponentString, frontMatterAndMarkdown.frontMatterObject]
 }
 
 const convertMarkdownAndHtmlToJsx = (markdownString: string): string => {
@@ -70,15 +101,23 @@ const convertMarkdownAndHtmlToJsx = (markdownString: string): string => {
     }
 }
 
-const extractFrontMatter = (stringWithFrontMatter: string, frontMatterMode:string, reactContextVarName?: string, reactContextName?: string): FrontMatterSplitFromMarkdown => {
+const extractFrontMatter = (stringWithFrontMatter: string, frontMatterMode: frontMatterModeEnum, reactContextVarName?: string, reactContextName?: string): FrontMatterSplitFromMarkdown => {
     try {
         const { data, content } = matter(stringWithFrontMatter)
-        const frontMatterJsxString = createFrontMatterJSXString(data, frontMatterMode, reactContextVarName, reactContextName)
 
-        const componentNameFromFrontMatter = data['react-component-name']
+        const frontMatterObject : any = frontMatterMode == frontMatterModeEnum.object 
+            ? data
+            : null 
+
+        const frontMatterJsxString = frontMatterMode == frontMatterModeEnum.reactHead || frontMatterMode == frontMatterModeEnum.reactHelmet
+            ? createFrontMatterJSXString(data, frontMatterMode, reactContextVarName, reactContextName)
+            : ''
+
+        const componentNameFromFrontMatter = data['react-component-name'] ?? data['title']
 
         return {
             frontMatterJsxString: frontMatterJsxString,
+            frontMatterObject: frontMatterObject,
             markdownString: content,
             componentNameFromFrontMatter
         }
@@ -88,15 +127,15 @@ const extractFrontMatter = (stringWithFrontMatter: string, frontMatterMode:strin
     }
 }
 
-const createFrontMatterJSXString = (propsToAssign: Object, frontMatterMode: string, reactContextVarName?: string, reactContextName?: string): string => {
+const createFrontMatterJSXString = (propsToAssign: Object, frontMatterMode: frontMatterModeEnum, reactContextVarName?: string, reactContextName?: string): string => {
     if(Object.keys(propsToAssign).length < 1)
         return ''
 
-    if(frontMatterMode == 'reacthead') {
+    if(frontMatterMode == frontMatterModeEnum.reactHead) {
         return createReactHeadString(propsToAssign, reactContextVarName as string, reactContextName as string)
     }
 
-    if(frontMatterMode == 'reacthelmet') {
+    if(frontMatterMode == frontMatterModeEnum.reactHelmet) {
         return createReactHelmentElements(propsToAssign)
     }
 
@@ -104,15 +143,15 @@ const createFrontMatterJSXString = (propsToAssign: Object, frontMatterMode: stri
 
 }
 
-const createJsxComponentString = (componentName: string, reactString: string, frontMatterMode:string, frontmatterString: string, reactContextName?: string): string => {
+const createJsxComponentString = (componentName: string, reactString: string, frontMatterMode:frontMatterModeEnum, frontmatterString?: string, reactContextName?: string): string => {
     const capitalizedMethodName = componentName.replace(/^\w/, c => c.toUpperCase())
     const camelCasedComponentName = componentName.replace(/^\w/, c => c.toLowerCase())
 
-    let reactComponent = `${frontmatterString ? frontMatterMode == 'reacthelmet' ? `import { Helmet } from 'react-helmet'\n\n` : `import { ${reactContextName} } from 'reactHead'\n\n` : ''}`
-    + `const ${capitalizedMethodName} = () => { \n  ${frontMatterMode == 'reacthelmet' ? '' : frontmatterString}
+    let reactComponent = `${frontmatterString ? frontMatterMode == frontMatterModeEnum.reactHelmet ? `import { Helmet } from 'react-helmet'\n\n` : `import { ${reactContextName} } from 'reactHead'\n\n` : ''}`
+    + `const ${capitalizedMethodName} = () => { \n  ${frontMatterMode == frontMatterModeEnum.reactHelmet ? '' : frontmatterString}
     return (
         <div className='${camelCasedComponentName}'>
-        ${frontmatterString && frontMatterMode != 'none' ? `${frontmatterString}\n\t\t` : ''}\t${reactString}
+        ${frontmatterString && frontMatterMode != frontMatterModeEnum.none ? `${frontmatterString}\n\t\t` : ''}\t${reactString}
         </div>
     )\n}
     \nexport default ${capitalizedMethodName}`
@@ -125,6 +164,15 @@ const createReactHelmentElements = (propsToAssign: Object) : string => {
     if(!propsToAssign) {
         return ''
     }
+
+    //Example
+    // <Helmet>
+    //     <meta name="date" content="2021-01-01" />
+    //     <title>node with react and redux</title>
+    //     <meta name="slug" content="node-react-redux" />
+    //     <meta name="description" content="How to node with react and redux" />
+    //     <meta name="tags" content="node,react,redux" />
+    // </Helmet>
 
     let propAssignmentString = ''
 
